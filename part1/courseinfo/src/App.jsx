c4d6bce18d1326c30cc2181ba8759e875feacc1c
Jeisson Fonseca <jstevenfm@gmail.com>
2024-08-21 21:07:49 -0500

    Cambiando el curso y sus partes a un solo objeto

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -39,26 +39,27 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <>
       <Header course={course} />
       <Content parts={parts} />
-      {/* <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} /> */}
       <Total parts={parts} />
     </>
   )
