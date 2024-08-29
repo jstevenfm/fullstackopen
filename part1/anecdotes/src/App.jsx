d66c1675af356d9f9b97b79f41652be7777cfb4d
Jeisson Fonseca <jstevenfm@gmail.com>
2024-08-28 21:13:45 -0500

    Inicializando el proyecto de anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      Button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Btn = ({ handleClick, title }) => <button onClick={handleClick}>{title}</button>
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <Btn handleClick={handleLeftClick} title='Left' />
+      <Btn handleClick={handleRightClick} title='Right' />
+      {right}
+      <History allClicks={allClicks}/>
+    </div>
+  )
+}
+
+export default App
