3a535559d67e29b6d6c8d67b2bd13dd1ac453391
Jeisson Fonseca <jstevenfm@gmail.com>
2024-08-28 21:32:22 -0500

    Solución 1.6: unicafe, paso 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,30 @@
 import { useState } from "react"
 
-const App = () => {
-  const [ counter, setCounter] = useState(0)
+const Btn = ({ onClick, title }) => <button onClick={onClick}>{title}</button>
 
-  const increaseOne = () => setCounter(counter + 1)
-  const decreaseOne = () => setCounter(counter - 1)
-  const resetCounter = () => setCounter(0)
+const Display = ({ title, counter }) => <p>{title}: {counter}</p>
+
+const App = () => {
+  const [ good, setGood] = useState(0)
+  const [ neutral, setNeutral] = useState(0)
+  const [ bad, setBad] = useState(0)
 
   return (
     <>
-      <Display counter={counter}/>
-      <Btn onClick={increaseOne} title='Increase one'/>
-      <Btn onClick={resetCounter} title='Reset counter'/>
-      <Btn onClick={decreaseOne} title='Decrease one'/>
+      <h1>Give Feedback</h1>
+      <Btn onClick={() => setGood(good + 1)} title="Good" />
+      <Btn onClick={() => setNeutral(neutral + 1)} title="Neutral" />
+      <Btn onClick={() => setBad(bad + 1)} title="Bad" />
+
+      <h2>Statistics</h2>
+      <Display title='Good' counter={good} />
+      <Display title='Neutral' counter={neutral} />
+      <Display title='Bad' counter={bad} />
     </>
   )
 }
 
-const Display = ({ counter }) => <div>{counter}</div>
 
-const Btn = ({ onClick, title }) => <button onClick={onClick}>{title}</button>
+
 
 export default App
