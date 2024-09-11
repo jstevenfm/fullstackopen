d9e48a25d12046dcea8ece3fe3cb7ceda20e4dbe
Jeisson Fonseca <jstevenfm@gmail.com>
2024-09-10 20:58:15 -0500

    Solución 1.7: unicafe, paso 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   const [ good, setGood] = useState(0)
   const [ neutral, setNeutral] = useState(0)
   const [ bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = all > 0 ? (good - bad) / all : 0
+  const positive = all > 0 ? (good * 100) / 9 : 0
 
   return (
     <>
@@ -20,6 +23,9 @@ const App = () => {
       <Display title='Good' counter={good} />
       <Display title='Neutral' counter={neutral} />
       <Display title='Bad' counter={bad} />
+      <Display title='All comments' counter={all} />
+      <Display title='Average' counter={average} />
+      <Display title='Positive' counter={positive} />
     </>
   )
 }
