dc65ff5052403e47019483a95f2297fe4d614f91
Jeisson Fonseca <jstevenfm@gmail.com>
2024-09-19 20:44:40 -0500

    Solución 1.11*: unicafe, paso 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,85 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Btn = ({ onClick, title }) => <button onClick={onClick}>{title}</button>
+const Btn = ({ onClick, title }) => <button onClick={onClick}>{title}</button>;
 
-const StatisticLine = ({ title, counter }) => <p>{title}: {counter}</p>
+const StatisticLine = ({ title, counter }) => (
+  <p>
+    {title} {counter}
+  </p>
+);
 
-const Statistics = ({ good, neutral, bad}) => {
-  const all = good + neutral + bad
-  const average = all > 0 ? (good - bad) / all : 0
-  const positive = all > 0 ? (good * 100) / 9 : 0
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = all > 0 ? (good - bad) / all : 0;
+  const positive = all > 0 ? (good * 100) / 9 : 0;
 
-  if(all === 0) {
-    return <p>No Feedback Given</p>
+  if (all === 0) {
+    return <p>No Feedback Given</p>;
   }
 
   return (
     <>
-      <StatisticLine title='Good' counter={good} />
-      <StatisticLine title='Neutral' counter={neutral} />
-      <StatisticLine title='Bad' counter={bad} />
-      <StatisticLine title='All comments' counter={all} />
-      <StatisticLine title='Average' counter={average} />
-      <StatisticLine title='Positive' counter={positive} />
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <StatisticLine title="Good" />
+            </td>
+            <td>
+              <StatisticLine counter={good} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine title="Neutral" />
+            </td>
+            <td>
+              <StatisticLine counter={neutral} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine title="Bad" />
+            </td>
+            <td>
+              <StatisticLine counter={bad} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine title="All comments" />
+            </td>
+            <td>
+              <StatisticLine counter={all} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine title="Average" />
+            </td>
+            <td>
+              <StatisticLine counter={average} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine title="Positive" />
+            </td>
+            <td>
+              <StatisticLine counter={positive} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      {/* */}
     </>
-  )
-}
+  );
+};
 
 const App = () => {
-  const [ good, setGood] = useState(0)
-  const [ neutral, setNeutral] = useState(0)
-  const [ bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <>
@@ -40,10 +91,7 @@ const App = () => {
       <h2>Statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </>
-  )
-}
+  );
+};
 
-
-
-
-export default App
+export default App;
