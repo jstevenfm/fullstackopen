a71eb768c2bcd150af05480c0c0947060946e2ec
Jeisson Fonseca <jstevenfm@gmail.com>
2024-09-19 21:32:20 -0500

    Resolviendo 1.12*: anecdotes, paso 1

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,45 +1,28 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        The app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      Button press history: {props.allClicks.join(' ')}
-    </div>
-  )
+const newAnecdote = () => {
+  return Math.floor(Math.random() * 8)
 }
 
-const Btn = ({ handleClick, title }) => <button onClick={handleClick}>{title}</button>
+const Btn = ({ onClick }) => <button onClick={onClick}>Next anecdote</button>
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
+  const [selected, setSelected] = useState(0)
   return (
     <div>
-      {left}
-      <Btn handleClick={handleLeftClick} title='Left' />
-      <Btn handleClick={handleRightClick} title='Right' />
-      {right}
-      <History allClicks={allClicks}/>
+      {anecdotes[selected]} <br />
+      <Btn onClick={() => setSelected(newAnecdote())} />
     </div>
   )
 }
