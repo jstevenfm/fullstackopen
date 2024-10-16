f8ac2d9d15d8f4cfa8ce99e2ee083e6dde5d4c75
Jeisson Fonseca <jstevenfm@gmail.com>
2024-10-15 21:36:13 -0500

    Solucionando 2.6: La Agenda Telefónica Paso 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    const nameObject = {name: newName}
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+    console.log('button clicked', event.target)
+  }
+
+  const Person = ({ person }) => <li>{person.name}</li>
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
+      <ul>
+        {persons.map(person =>
+          <Person key={person.name} person={person} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
