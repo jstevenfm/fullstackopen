d644c7cec4249a17fa761f502b8fa353babb52ac
Jeisson Fonseca <jstevenfm@gmail.com>
2024-10-22 20:54:42 -0500

    Resolviendo 2.8: La Agenda Telefónica Paso 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,19 +2,26 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-123456',
+      id: 1
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const ValidatePerson = (event) =>{
     event.preventDefault()
     const found = (person) => person.name === newName
     const foundPerson = persons.some(found)
-    console.log(foundPerson);
     if (foundPerson) {
       return alert(`${newName} is already added to phonebook`)
     }
@@ -23,31 +30,34 @@ const App = () => {
   
   const addName = () => {
 
-    const nameObject = {name: newName}
+    const nameObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
 
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
-  const Person = ({ person }) => <li>{person.name}</li>
+  const Person = ({ person }) => <li>{person.name} {person.number}</li>
 
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={ValidatePerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input type='tel' value={newNumber} onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
       <ul>
         {persons.map(person =>
-          <Person key={person.name} person={person} />
+          <Person key={person.id} person={person} />
         )}
       </ul>
     </div>
